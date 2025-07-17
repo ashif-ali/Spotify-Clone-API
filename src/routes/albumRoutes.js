1f11974608461ca58bfc41b2a1a83df6e8c8714c
Sk Md Ashif Ali <36637666+ashif-ali@users.noreply.github.com>
2025-07-17 10:03:01 +0530

    Dummy controllers and routes for album

---
+++ src/routes/albumRoutes.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const {
+    createAlbum,
+    getAlbums,
+    getAlbumById,
+    updateAlbum,
+    deleteAlbum,
+    addSongsToAlbum,
+    removeSongFromAlbum,
+    getNewReleases,
+} = require("../controllers/albumController");
+const albumRouter = express.Router();
+const { protect, isAdmin } = require("../middlewares/auth");
+const upload = require("../middlewares/upload");
+
+//Public routes
+
+albumRouter.get("/", getAlbums);
+albumRouter.get("/new-releases", getNewReleases);
+albumRouter.get("/:id", getAlbumById);
+
+//Private routes
+albumRouter.post(
+    "/",
+    protect,
+    isAdmin,
+    upload.single("coverImage"),
+    createAlbum
+); //its get in vid
+
+albumRouter.put(
+    "/:id",
+    protect,
+    isAdmin,
+    upload.single("coverImage"),
+    updateAlbum
+);
+
+albumRouter.delete("/:id", protect, isAdmin, deleteAlbum);
+
+albumRouter.post("/:id/add-songs", protect, isAdmin, addSongsToAlbum);
+
+albumRouter.delete(
+    "/:id/remove-songs/:songId",
+    protect,
+    isAdmin,
+    removeSongFromAlbum
+);
+
+module.exports = albumRouter;
