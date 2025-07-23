6616351975035aed1138096b98c5fcdbabe30520
Sk Md Ashif Ali <36637666+ashif-ali@users.noreply.github.com>
2025-07-23 19:23:12 +0530

    implemented create song function

--- src/controllers/songController.js
+++ src/controllers/songController.js
@@ -11,7 +11,70 @@ const uploadToCloudinary = require("../utils/cloudinaryUpload");
  * @Access - Private/admin
  */
 
-const createSong = asyncHandler(async (req, res) => {});
+const createSong = asyncHandler(async (req, res) => {
+    const {title, artistId, albumId, duration, genre, isExplicit, featuredArtists} = req.body;
+
+    //Check if artist exists
+    const artist = await Artist.findById(artistId);
+    if (!artist) {
+        res.status(StatusCodes.NOT_FOUND);
+        throw new Error("Artist not found");
+    }
+    //Check if album exists if albumId is provided
+    if(albumId) {
+        const album = await Album.findById(albumId);
+        if (!album) {
+            res.status(StatusCodes.NOT_FOUND);
+            throw new Error("Album not found");
+        }
+    }
+
+    //Upload audio file
+    if(!req.files || !req.files.audio) {
+        res.status(StatusCodes.BAD_REQUEST);
+        throw new Error("Audio file is required");
+    }
+    const audioResult = await uploadToCloudinary(req.files.audio[0].path, "spotify/songs")
+
+    //Upload cover image if provided
+    let coverImageUrl = "";
+    if(req.files && req.files.cover) {
+        const imageResult = await uploadToCloudinary(req.files.cover, "spotify/covers")
+
+        coverImageUrl = imageResult.secure_url;
+    }
+    // Create new song
+    const newSong = new Song({
+        title,
+        artist: artistId,
+        album: albumId || null,
+        duration,
+        genre,
+        isExplicit: isExplicit || false,
+        featuredArtist: featuredArtists ? JSON.parse(featuredArtists) : [],
+        audioUrl: audioResult.secure_url,
+        coverImage: coverImageUrl
+    })
+
+    //Save song to database
+    await newSong.save();
+
+    //Add song to artist's songs
+    artist.songs.push(newSong._id);
+    await artist.save();
+
+    //add song to album if albumId is provided
+    if (albumId) {
+        const album = await Album.findById(albumId);
+        album.songs.push(newSong._id);
+        await album.save();
+    }
+    res.status(StatusCodes.CREATED).json({
+        message: "Song created successfully",
+        song: newSong,
+    });
+    
+});
 
 /**
  * @desc - Get all songs with filtering and pagination
